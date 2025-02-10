e84e231f3a21aba95398f5bcd28cb4ac8a7b8cb0
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-02-11 01:22:20 +0900

    save

--- client.cpp
+++ client.cpp
@@ -1,22 +1,14 @@
 #include <Novice.h>
 #include <math.h>
-#include <fstream>
-#include <process.h>
 #include <mmsystem.h>
+#include <process.h>
 
 #pragma comment(lib, "wsock32.lib")
 #pragma comment(lib, "winmm.lib")
 
 DWORD WINAPI threadfunc(void*);
-
-struct POS
-{
-	int x;
-	int y;
-};
-POS pos1P, pos2P, old_pos2P;
-RECT rect;
-SOCKET sWait;
+SOCKET sock;
+bool bSocket = false;
 HWND hwMain;
 
 const char kWindowTitle[] = "KAMATA ENGINEクライアント";
@@ -24,18 +16,18 @@ const char kWindowTitle[] = "KAMATA ENGINEクライアント";
 typedef struct {
 	float x;
 	float y;
-}Vector2;
+} Vector2;
 
 typedef struct {
 	Vector2 center;
 	float radius;
-}Circle;
+} Circle;
 
 // キー入力結果を受け取る箱
 Circle a, b;
-Vector2 center = { 100,100 };
-char keys[256] = { 0 };
-char preKeys[256] = { 0 };
+Vector2 center = {100, 100};
+char keys[256] = {0};
+char preKeys[256] = {0};
 int color = RED;
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -44,7 +36,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WSADATA wdData;
 	static HANDLE hThread;
 	static DWORD dwID;
-
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 800, 600);
@@ -64,7 +55,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// winsock初期化
 	WSAStartup(MAKEWORD(2, 0), &wdData);
 
-	// データを送受信処理をスレッド（WinMainの流れに関係なく動作する処理の流れ）として生成
+	// データを送受信処理をスレッド（WinMainの流れに関係なく動作する処理の流れ）として生成。
+	// データ送受信をスレッドにしないと何かデータを受信するまでRECV関数で止まってしまう。
 	hThread = (HANDLE)CreateThread(NULL, 0, &threadfunc, (LPVOID)&a, 0, &dwID);
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -89,21 +81,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			a.center.x -= 5;
 		}
 
+		///
 		/// ↓更新処理ここから
-		float distance =
-			sqrtf((float)pow((double)a.center.x - (double)b.center.x, 2) +
-				  (float)pow((double)a.center.y - (double)b.center.y, 2));
+		///
 
-		if (distance <= a.radius+b.radius)
+		float distance = sqrtf((float)pow((double)a.center.x - (double)b.center.x, 2) + (float)pow((double)a.center.y - (double)b.center.y, 2));
+
+		if (distance <= a.radius + b.radius) {
 			color = BLUE;
-		else
+		} else
 			color = RED;
+		///
 		/// ↑更新処理ここまで
+		///
 
+		///
 		/// ↓描画処理ここから
+		///
 		Novice::DrawEllipse((int)a.center.x, (int)a.center.y, (int)a.radius, (int)a.radius, 0.0f, WHITE, kFillModeSolid);
-		Novice::DrawEllipse((int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius,0.0f, color, kFillModeSolid);
+		Novice::DrawEllipse((int)b.center.x, (int)b.center.y, (int)b.radius, (int)b.radius, 0.0f, color, kFillModeSolid);
+		///
 		/// ↑描画処理ここまで
+		///
 
 		// フレームの終了
 		Novice::EndFrame();
@@ -118,63 +117,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Finalize();
 
 	// winsock終了
+	closesocket(sock);
 	WSACleanup();
 
 	return 0;
 }
 
 // 通信スレッド関数
-DWORD WINAPI threadfunc(void* test) {
+DWORD WINAPI threadfunc(void* p) {
 
-	SOCKET sConnect;
-	struct sockaddr_in saConnect, saLocal;
-	DWORD dwAddr;
-	char addr[20]; // IPアドレス格納配列
+	p;
 
-	test = 0; //引数確認
-	WORD wPort = 8000;
+	int recv_cnt;
+	int send_cnt;
+	int fromlen;
+	struct sockaddr_in send_addr, recv_addr;
 
-	// ファイル読み込み (#include <fstream> が必要)
-	std::ifstream ifs("ip.txt");
-	ifs.getline(addr, sizeof(addr));
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
 
-	dwAddr = inet_addr(addr);
-	sConnect = socket(PF_INET, SOCK_STREAM, 0);
+	ZeroMemory(&send_addr, sizeof(sockaddr_in));
+	ZeroMemory(&recv_addr, sizeof(sockaddr_in));
 
-	ZeroMemory(&saConnect, sizeof(sockaddr_in));
-	ZeroMemory(&saLocal, sizeof(sockaddr_in));
+	// ソケットにポート番号と送信先IPアドレスを設定
+	send_addr.sin_family = AF_INET;
+	send_addr.sin_port = htons(8000);
+	send_addr.sin_addr.s_addr = inet_addr("192.168.3.4");
 
-	saLocal.sin_family = AF_INET;
-	saLocal.sin_addr.s_addr = INADDR_ANY;
-	saLocal.sin_port = 0;
+	while (1) {
+		recv_cnt = 0;
+		send_cnt = 0;
+		fromlen = sizeof(recv_addr);
 
-	bind(sConnect, (LPSOCKADDR)&saLocal, sizeof(saLocal));
-
-	saConnect.sin_family = AF_INET;
-	saConnect.sin_addr.s_addr = dwAddr;
-	saConnect.sin_port = htons(wPort);
-
-	// サーバーに接続
-	if (connect(sConnect, (sockaddr*)(&saConnect), sizeof(saConnect)) == SOCKET_ERROR)
-	{
-		closesocket(sConnect);
-		WSACleanup();
-		return 1;
-	}
-
-	while (1)
-	{
-		// 2P（クライアント側）の座標情報を送信
-		send(sConnect, (const char*)&a, sizeof(Circle), 0);
+		// データ送信
+		while (send_cnt == 0) {
+			send_cnt = sendto(sock, (const char*)&a, sizeof(Circle), 0, (struct sockaddr*)&send_addr, sizeof(send_addr));
+		}
 
 		// サーバから1P座標情報を取得
-		int nRcv = recv(sConnect, (char*)&b, sizeof(Circle), 0);
-
-		// 通信途絶えたらループ終了
-		if (nRcv == SOCKET_ERROR)break;
+		recv_cnt = recvfrom(sock, (char*)&b, sizeof(Circle), 0, (struct sockaddr*)&recv_addr, &fromlen);
 	}
-
-	closesocket(sConnect);
 
 	return 0;
 }
